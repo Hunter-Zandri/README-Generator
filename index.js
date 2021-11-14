36a1582eb02b249659f675c3d614751b1b8261aa
Hunter-Zandri <86785668+Hunter-Zandri@users.noreply.github.com>
2021-11-14 11:35:07 -0500

    generator works as it should, added read.me file

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const fs = require('fs');
 
 inquirer.prompt([
     {
-        message: 'What is your project title?',
+        message: 'What is your name?',
         type: 'input',
         name: 'title'
     },
     {
-        message: 'Enter your description: ',
+        message: 'What is the title of your project?',
+        type: 'input',
+        name: 'title'
+    },
+    {
+        message: 'Describe your project',
         type: 'input',
         name: 'description'
     },
     {
-        message: 'Enter your installation instructions:',
+        message: 'Installation instructions:',
         default: 'npm i',
         type: 'input',
         name: 'installation'
     },
     {
-        message: 'Enter your usage information: ',
+        message: 'Usage information: ',
         type: 'input',
         name: 'usage'
     },
@@ -51,14 +56,61 @@ inquirer.prompt([
         name: 'email',
     },
 ]).then((data) => {
+    let license;
+    let link;
+    switch (data.license) {
+
+        case 'ISC': 
+        license = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue)](https://opensource.org/licenses/ISC)';
+        link = 'https://opensource.org/licenses/ISC'
+            break;
+        case 'MIT':
+        license = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow)](https://opensource.org/licenses/MIT)';
+        link = 'https://opensource.org/licenses/MIT'
+            break;
+        case 'GPL': 
+        license = '[![License: GPL](https://img.shields.io/badge/License-GPL-blue)](https://opensource.org/licenses/AGPL-3.0)';
+        link = 'https://opensource.org/licenses/AGPL-3.0'
+            break;
+        default:
+            license = '';
+            break;
+    }
     const string = `
     
 # ${data.title}
 ${license}
 ## Description
 ${data.description}
-\
+## Table of Contents
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+- [Video](#video)
+## Installation Instructions
+\`\`\`
+${data.installation}
+\`\`\`
+## Usage Information
+${data.usage}
+## Contribution Guidelines
+${data.contribution}
+## Test Instructions
+\`\`\`
+${data.test}
+\`\`\`
+## License
+This project is licensed under the terms of the [${data.license}](${link}) license
+## Questions
+My Github Profile: [${data.github}](https://github.com/${data.github})
+Email me your questions at: [${data.email}](mailto:${data.email})
+## Video
+Watch this for a guide on this project: [Video]()
 `
+
+
     fs.writeFile("README.md",  string, (err) =>
     err? console.error(err) : console.log("created!")
 )
